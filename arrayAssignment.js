2329f7203d407a6343af8916e45a8423b0a7d31e
sanjayal <44019615+sanjayyaldandi@users.noreply.github.com>
2018-10-16 21:11:26 +0530

    add reverse fibonacci function.
    reverse fibonacci function takes a number as an input and returns
    fibonacci series upto given limit in reversed order.

--- arrayAssignment.js
+++ arrayAssignment.js
@@ -12,4 +12,78 @@ const filterOddNumbers = function(numbers){
   return oddNumbers;
 }
 
+//--------------------------------------------------------
+
+const isEven = function(number){
+  return (number % 2 == 0);
+}
+
+const filterEvenNumbers = function(numbers){
+  let evenNumbers = [];
+  for(let number of numbers){
+    if( isEven(number) ){
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
+}
+
+//--------------------------------------------------------
+
+const sumOfElements = function(numbers){
+  let sum = [];
+  let addition = 0;
+  for(let number of numbers){
+    addition += number;
+  }
+  sum.push(addition);
+  return sum;
+}
+
+//--------------------------------------------------------
+
+const printReverse = function(numbers){
+  let reversedArray = [];
+  for(let number of numbers){
+    reversedArray.unshift(number);
+  }
+  return reversedArray;
+}
+
+//--------------------------------------------------------
+
+const filterAlternatingElements = function(numbers){
+  let filteredNums = [];
+  for(let number=0;number<numbers.length;number+=2){
+    filteredNums.push(numbers[number]);
+  }
+  return filteredNums;
+}
+
+//--------------------------------------------------------
+
+const reverseFibo = function(givenNumber){
+  let reversedFiboSeries = [];
+
+  let num1 = -1;
+  let num2 = 1;
+  let sum = 0;
+
+  for(let number=0;number<givenNumber;number++){
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+
+    reversedFiboSeries.unshift(sum);
+  }
+  return reversedFiboSeries;
+}
+
+//--------------------------------------------------------
+
 exports.filterOddNumbers = filterOddNumbers;
+exports.filterEvenNumbers = filterEvenNumbers;
+exports.sumOfElements = sumOfElements;
+exports.printReverse = printReverse;
+exports.filterAlternatingElements = filterAlternatingElements;
+exports.reverseFibo = reverseFibo;
--- arrayAssignmentTest.js
+++ arrayAssignmentTest.js
@@ -1,4 +1,63 @@
 let assert = require("assert");
-let filterOddNumbers = require("./arrayAssignment").filterOddNumbers;
+let lib = require("./arrayAssignment");
 
-assert.deepEqual(filterOddNumbers([2,3,5,7]),[3,5,7]);
+//----------------------------------------------------------
+
+let filterOddNumbers = lib.filterOddNumbers;
+
+let inputOdd = [[],[1],[2],[1,1],[2,2],[2,1],[1,2],[-1],[-2],[-1,-1],[-2,-2],[-2,-1],[-1,-2]];
+let expectedOddOutput = [[],[1],[],[1,1],[],[1],[1],[-1],[],[-1,-1],[],[-1],[-1]];
+
+for(let index in inputOdd){
+    assert.deepEqual(filterOddNumbers(inputOdd[index]),expectedOddOutput[index]);
+}
+
+//----------------------------------------------------------
+
+let filterEvenNumbers = lib.filterEvenNumbers;
+
+let inputEven = [[],[1],[2],[1,1],[2,2],[2,1],[1,2],[-1],[-2],[-1,-1],[-2,-2],[-2,-1],[-1,-2]];
+let expectedEvenOutput = [[],[],[2],[],[2,2],[2],[2],[],[-2],[],[-2,-2],[-2],[-2]];
+
+for(let index in inputEven){
+  assert.deepEqual(filterEvenNumbers(inputEven[index]),expectedEvenOutput[index]);
+}
+
+//----------------------------------------------------------
+
+let sumOfElements = lib.sumOfElements;
+
+let inputSum =          [[1],[2],[1,1],[2,2],[2,1],[1,2],[-1],[-2],[-1,-1],[-2,-2],[-2,-1],[-1,-2]];
+let expectedSumOutput = [[1],[2],[2],  [4],  [3],  [3],  [-1],[-2],[-2],   [-4],   [-3],   [-3]];
+
+for(let index in inputSum){
+  assert.deepEqual(sumOfElements(inputSum[index]),expectedSumOutput[index]);
+}
+
+//----------------------------------------------------------
+
+let printReverse = lib.printReverse;
+
+let inputPrintReverse =     [[],[1],[1,2],[2,1],[1,2,3],[3,2,1]];
+let expectedReverseOutput = [[],[1],[2,1],[1,2],[3,2,1],[1,2,3]];
+
+for(let index in inputPrintReverse){
+  assert.deepEqual(printReverse(inputPrintReverse[index]),expectedReverseOutput[index]);
+}
+
+//----------------------------------------------------------
+
+let filterAlternatingElements = lib.filterAlternatingElements;
+
+assert.deepEqual(filterAlternatingElements([1,2,3,4]),[1,3]);
+assert.deepEqual(filterAlternatingElements([7,5,3,5,4,3]),[7,3,4]);
+
+//----------------------------------------------------------
+
+let reverseFibo = lib.reverseFibo;
+
+assert.deepEqual(reverseFibo(5),[3,2,1,1,0])
+assert.deepEqual(reverseFibo(8),[13,8,5,3,2,1,1,0])
+
+//----------------------------------------------------------
+
